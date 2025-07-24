8d960535304441147df9804d77591e900deda45e
Jose Amadeo Diaz <jamdiazdiaz@gmail.com>
2025-07-24 10:44:39 -0500

    demo native

---
+++ 04-demo-native-start/demo-native/src/test/java/com/bcp/NativeExpenseResourceIT.java
@@ -0,0 +1,9 @@
+package com.bcp;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NativeExpenseResourceIT extends ExpenseResourceTest {
+
+    // Execute the same tests but in native mode.
+}
