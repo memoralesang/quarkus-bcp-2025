39413b22429b69501996a4f21f77e67aeca2783a
Jose Diaz <jose.diaz@joedayz.pe>
2025-07-30 11:54:25 -0500

    mock start test

---
+++ 04-demo-test-start/test-mock/src/main/java/com/bcp/FraudScore.java
@@ -0,0 +1,9 @@
+package com.bcp;
+
+public class FraudScore {
+    public int score;
+
+    public FraudScore(int score) {
+        this.score = score;
+    }
+}
---
+++ 04-demo-test-start/test-mock/src/test/java/com/bcp/SpyTest.java
@@ -0,0 +1,7 @@
+package com.bcp;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class SpyTest {
+}
