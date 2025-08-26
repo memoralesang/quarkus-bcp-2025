303aa2cfe3235ba7a7c3baeabd55d55ed9f39e0e
Jose Diaz <jose.diaz@joedayz.pe>
2025-08-25 21:30:14 -0500

    persist start

--- 03-demo-persist-start/expense-service/src/main/resources/import.sql
+++ 03-demo-persist-start/expense-service/src/main/resources/import.sql
@@ -1,6 +1,36 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+
+insert into Associate (id, name)
+values (1, 'Jaime');
+insert into Associate (id, name)
+values (2, 'Pablo');
+
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Desk', '0','150.50', 1);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Online Learning', '1','75.00', 1);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Books', '0','50.00', 1);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Internet', '1','20.00', 1);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Phone', '0','15.00', 1);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Bookshelf', '0','150.50', 1);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Printer Cartridges', '1','15.00', 2);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Online Learning', '0','50.00', 2);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Internet', '1','20.00', 2);
+
+insert into Expense (uuid, id, name, paymentmethod, amount, associate_id)
+values (gen_random_uuid(), nextval('Expense_SEQ'), 'Phone', '0','15.00', 2);
