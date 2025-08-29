5ae826b3612a18e48fc8591ebc94d893960f7e7b
Jose Diaz <jose.diaz@joedayz.pe>
2025-08-29 08:47:28 -0500

    monitor start

---
+++ 10-tolerance-policies-start/monitor/src/main/java/com/bcp/training/cpu/CpuStats.java
@@ -0,0 +1,16 @@
+package com.bcp.training.cpu;
+
+import java.util.List;
+
+public class CpuStats {
+
+    public List<Double> usageTimeSeries;
+    public Double mean;
+    public Double standardDeviation;
+
+    public CpuStats(List<Double> usageTimeSeries, Double mean, Double standardDeviation) {
+        this.usageTimeSeries = usageTimeSeries;
+        this.mean = mean;
+        this.standardDeviation = standardDeviation;
+    }
+}
