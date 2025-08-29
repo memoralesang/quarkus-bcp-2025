2eb1ae747f12f49d0d93f23dae6750b9083b2c98
Jose Diaz <jose.diaz@joedayz.pe>
2025-08-29 09:01:47 -0500

    review tolerance

---
+++ 10-tolerance-review-solution/session/src/main/java/com/bcp/training/conference/session/SpeakerRepository.java
@@ -0,0 +1,9 @@
+package com.bcp.training.conference.session;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+@ApplicationScoped
+public class SpeakerRepository implements PanacheRepository<Speaker> {
+
+}
---
+++ 10-tolerance-review-start/session/src/main/java/com/bcp/training/conference/session/ReadinessCheck.java
@@ -0,0 +1,8 @@
+package com.bcp.training.conference.session;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ReadinessCheck {
+}
